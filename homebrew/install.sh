5821f5acae9e0e1fa4d69e8f4c2b752ccdbc81f6
Nick Dunkman <nickdunkman@gmail.com>
2020-08-07 17:00:47 -0700

    "lock" homebrew installations into a Brewfile

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -11,10 +11,4 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew install git autojump grc coreutils
-
-# install nerd fonts (for powerlevel10k prompt)
-brew tap homebrew/cask-fonts
-brew cask install font-meslo-for-powerline
-
-exit 0
+brew bundle --file=$DOTFILES/homebrew/Brewfile --no-lock
