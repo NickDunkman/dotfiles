833fa6e819613b2a3d5667f191cdecbb703bc916
Nick Dunkman <nickdunkman@gmail.com>
2020-08-19 15:57:37 -0700

    docker shortcuts

---
+++ docker/aliases.zsh
@@ -0,0 +1,2 @@
+alias "wtf"='docker stop $(docker ps -qa); docker rm $(docker ps -qa); docker rmi $(docker images -f dangling=true -q)'
+alias "WTF"='docker stop $(docker ps -a -q); docker network prune -f; ! docker rm $(docker ps -a -q); docker volume rm $(docker volume ls -f dangling=true -q); docker rmi $(docker images -f dangling=true -q); docker rmi $(docker images -a -q)'
