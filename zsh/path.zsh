df2334976badc42d004b8df69865ef177120b44d
Nick Dunkman <nickdunkman@gmail.com>
2020-08-21 14:58:40 -0700

    more sensible way of building the path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,8 +1,13 @@
-export PATH="$DOTFILES/bin\
-:$HOME/.pyenv/bin\
-:$HOME/.poetry/bin\
-:/usr/local/bin\
-:/usr/bin\
-:/bin\
-:/usr/sbin\
-:/sbin"
+# Highest precedence first
+paths=(
+  $DOTFILES/bin
+  $HOME/.pyenv/bin
+  $HOME/.poetry/bin
+  /usr/local/bin
+  /usr/bin
+  /bin
+  /usr/sbin
+  /sbin
+)
+
+export PATH=${(j[:])paths}
