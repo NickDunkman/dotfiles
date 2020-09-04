fb4801a0d0484d398bb5cf61162552f28b871a32
Nick Dunkman <nickdunkman@gmail.com>
2020-09-04 10:27:24 -0700

    more Makefile shortcuts

--- make/aliases.zsh
+++ make/aliases.zsh
@@ -1,1 +1,8 @@
+alias mb="make build"
+alias mba="make build_acceptance"
+alias mjs="sh -c \"make js_shell\""
+alias mjt="make js_test"
+alias mr="make run"
 alias ms="sh -c \"make shell\""
+alias mt="make test"
+alias mta="make test_acceptance"
