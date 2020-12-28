617b78ea8700d316606f5989f83e92b69689f76c
Nick Dunkman <nickdunkman@gmail.com>
2020-12-28 15:41:43 -0800

    Finder defaults

---
+++ finder/defaults.sh
@@ -0,0 +1,5 @@
+# show hidden files in Finder
+defaults write com.apple.Finder AppleShowAllFiles true
+
+# Always use column-view in Finder
+defaults write com.apple.Finder FXPreferredViewStyle clmv
