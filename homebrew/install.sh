52f9f16f98c7e8d84b1430329f1788922732f2f8
Nick Dunkman <nickdunkman@gmail.com>
2021-03-03 14:14:57 -0800

    always be upgradin those brews

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -5,4 +5,6 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+brew update
 brew bundle --file=$DOTFILES/homebrew/Brewfile --no-lock
+brew upgrade
