d0f856b444f1d1031c4c63c9d6dc283f24e1bf5f
Nick Dunkman <nickdunkman@gmail.com>
2021-07-19 23:51:49 -0700

    this no longer works

--- finder/defaults.sh
+++ finder/defaults.sh
@@ -1,5 +1,2 @@
 # show hidden files in Finder
 defaults write com.apple.Finder AppleShowAllFiles true
-
-# Always use column-view in Finder
-defaults write com.apple.Finder FXPreferredViewStyle clmv
