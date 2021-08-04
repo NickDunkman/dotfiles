b773956c446818e180721fac8783d6a781b36590
Nick Dunkman <nickdunkman@gmail.com>
2021-08-03 18:14:00 -0700

    permanently fix these c compilation issues 🙏

--- python/config.zsh
+++ python/config.zsh
@@ -5,3 +5,6 @@ export PYENV_ROOT=$HOME/.pyenv
 if command -v pyenv 1>/dev/null 2>&1; then
     eval "$(pyenv init --path)"
 fi
+
+export LDFLAGS="-L$(brew --prefix openssl)/lib"
+export CFLAGS="-I$(brew --prefix openssl)/include" 
