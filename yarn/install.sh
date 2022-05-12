7bd5ced77f292376cc3cffe577ec472e10d847da
Nick Dunkman <nickdunkman@gmail.com>
2022-05-12 15:14:51 -0700

    don't need any of these things installed globally anymore

--- yarn/install.sh
+++ yarn/install.sh
@@ -1,16 +1,3 @@
 if ! type "yarn" > /dev/null; then
   npm install --global corepack
 fi
-
-yarn global add eslint@7.21.0
-yarn global add eslint-plugin-import@2.22.1
-yarn global add eslint-plugin-react@7.22.0
-yarn global add eslint-plugin-jsx-a11y@6.4.1
-yarn global add eslint-plugin-flowtype@2.40.1
-yarn global add eslint-config-airbnb@18.2.1
-yarn global add eslint-config-airbnb-base@14.2.1
-yarn global add babel-eslint@10.1.0
-yarn global add eslint-config-prettier@2.9.0
-yarn global add eslint-plugin-prettier@3.3.1
-yarn global add prettier@2.2.1
-yarn global add eslint-plugin-react-hooks@4.2.0
