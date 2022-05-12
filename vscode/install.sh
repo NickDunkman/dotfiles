2485354b7cae5ae451f3f36ee8447e1b8e5504a1
Nick Dunkman <nickdunkman@gmail.com>
2022-05-12 13:26:11 -0700

    update vscode extensions list

--- vscode/install.sh
+++ vscode/install.sh
@@ -7,7 +7,6 @@ ln -s $DOTFILES/vscode/settings.json ~/Library/Application\ Support/Code/User/se
 #   code --list-extensions | xargs -L 1 echo code --install-extension
 
 code --install-extension ashpowell.monokai-one-dark-vivid
-code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension dotiful.dotfiles-syntax-highlighting
 code --install-extension DotJoshJohnson.xml
@@ -17,11 +16,12 @@ code --install-extension esbenp.prettier-vscode
 code --install-extension mechatroner.rainbow-csv
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
 code --install-extension oderwat.indent-rainbow
 code --install-extension redhat.vscode-yaml
 code --install-extension shd101wyy.markdown-preview-enhanced
-code --install-extension SonarSource.sonarlint-vscode
-code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension silvenon.mdx
+code --install-extension stylelint.vscode-stylelint
 code --install-extension vscodevim.vim
 code --install-extension waderyan.gitblame
 code --install-extension yzhang.markdown-all-in-one
