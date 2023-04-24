2f657de3e427041bf5fe49101bbdb1540441be9a
Nick Dunkman <nickdunkman@gmail.com>
2023-04-24 12:45:34 -0700

    add more finder settings

--- finder/defaults.sh
+++ finder/defaults.sh
@@ -1,2 +1,14 @@
-# show hidden files in Finder
+# show extensions
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+
+# show hidden files
 defaults write com.apple.Finder AppleShowAllFiles true
+
+# use column view
+defaults write com.apple.finder "FXPreferredViewStyle" -string "clmv"
+
+# sort folders to top
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "false"
+
+# don't warn when changing extension
+defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
