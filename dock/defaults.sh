a5c62d2ed0496390d787f921b9f77cb021c7d771
Nick Dunkman <nickdunkman@gmail.com>
2023-04-24 12:45:27 -0700

    add dock settings

---
+++ dock/defaults.sh
@@ -0,0 +1,5 @@
+# hide the dock unless mousing over
+defaults write com.apple.dock "autohide" -bool "true"
+
+# set dock size to largest possible (optimize for ease of clicking)
+defaults write com.apple.dock "tilesize" -int "128"
