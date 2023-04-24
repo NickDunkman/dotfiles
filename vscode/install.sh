25510699077ad4ae24e33fe1ceb72d5c3b840c53
Nick Dunkman <nickdunkman@gmail.com>
2023-04-24 12:47:42 -0700

    do all VSCode extension installs in one command for nicer output

--- vscode/install.sh
+++ vscode/install.sh
@@ -4,22 +4,23 @@ ln -s $DOTFILES/vscode/settings.json ~/Library/Application\ Support/Code/User/se
 # Can generate this list using:
 #   code --list-extensions | xargs -L 1 echo code --install-extension
 
-code --install-extension ashpowell.monokai-one-dark-vivid --force
-code --install-extension dbaeumer.vscode-eslint --force
-code --install-extension dotiful.dotfiles-syntax-highlighting --force
-code --install-extension DotJoshJohnson.xml --force
-code --install-extension eamodio.gitlens --force
-code --install-extension eriklynd.json-tools --force
-code --install-extension esbenp.prettier-vscode --force
-code --install-extension mechatroner.rainbow-csv --force
-code --install-extension ms-azuretools.vscode-docker --force
-code --install-extension ms-python.python --force
-code --install-extension ms-python.vscode-pylance --force
-code --install-extension oderwat.indent-rainbow --force
-code --install-extension redhat.vscode-yaml --force
-code --install-extension shd101wyy.markdown-preview-enhanced --force
-code --install-extension unifiedjs.vscode-mdx --force
-code --install-extension stylelint.vscode-stylelint --force
-code --install-extension vscodevim.vim --force
-code --install-extension waderyan.gitblame --force
-code --install-extension yzhang.markdown-all-in-one --force
+code \
+    --install-extension ashpowell.monokai-one-dark-vivid --force \
+    --install-extension dbaeumer.vscode-eslint --force \
+    --install-extension dotiful.dotfiles-syntax-highlighting --force \
+    --install-extension DotJoshJohnson.xml --force \
+    --install-extension eamodio.gitlens --force \
+    --install-extension eriklynd.json-tools --force \
+    --install-extension esbenp.prettier-vscode --force \
+    --install-extension mechatroner.rainbow-csv --force \
+    --install-extension ms-azuretools.vscode-docker --force \
+    --install-extension ms-python.python --force \
+    --install-extension ms-python.vscode-pylance --force \
+    --install-extension oderwat.indent-rainbow --force \
+    --install-extension redhat.vscode-yaml --force \
+    --install-extension shd101wyy.markdown-preview-enhanced --force \
+    --install-extension unifiedjs.vscode-mdx --force \
+    --install-extension stylelint.vscode-stylelint --force \
+    --install-extension vscodevim.vim --force \
+    --install-extension waderyan.gitblame --force \
+    --install-extension yzhang.markdown-all-in-one --force
