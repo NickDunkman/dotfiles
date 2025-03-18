2aace4d8bcdcd2f4060d4e2e124956abf8afc76e
Nick Dunkman <nickdunkman@gmail.com>
2025-03-18 13:17:22 -0700

    remove this

--- system/defaults.sh
+++ system/defaults.sh
@@ -1,3 +1,4 @@
 # holding down a key should repeatedly press that key
 # (instead of accents menu)
-defaults write NSGlobalDomain "ApplePressAndHoldEnabled" -bool "false"
+#defaults write NSGlobalDomain "ApplePressAndHoldEnabled" -bool "false"
+# ^ disabling this for now so that I can use the accents menu for French stuff!
