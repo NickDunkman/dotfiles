b324eab2870055d71fff1ad109ddd87450c94222
Nick Dunkman <nickdunkman@gmail.com>
2025-03-20 11:19:49 -0700

    update

--- vscode/defaults.sh
+++ vscode/defaults.sh
@@ -1,4 +1,10 @@
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
-defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
-defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
-#defaults delete -g ApplePressAndHoldEnabled                                      # If necessary, reset global default
+defaults delete -g ApplePressAndHoldEnabled                                       # reset global default
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false          # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false  # For VS Code Insider
+defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false     # For VS Codium
+
+
+
+
+
+
