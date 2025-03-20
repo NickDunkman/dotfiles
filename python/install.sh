dbc6332080430d5420dffeae6e8c579b0149b3b9
Nick Dunkman <nickdunkman@gmail.com>
2025-03-20 12:27:45 -0700

    update python install stuff

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,16 @@
+GLOBAL_PYTHON_VERSION=3.13
+
+pyenv install $GLOBAL_PYTHON_VERSION --skip-existing
+
+# This updates ~/.pyenv/version to point at the specified version. This means
+# pyenv will use this version everywhere unless in a subdirectory which overrides
+# to a different version.
+pyenv global $GLOBAL_PYTHON_VERSION
+
 if ! type "poetry" > /dev/null; then
-  curl -sSL https://install.python-poetry.org | python -
+  pipx install poetry
+else
+  pipx upgrade poetry
 fi
 
 pyenv rehash
