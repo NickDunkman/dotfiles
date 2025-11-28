a4494d3f146e3c2fcb444a8b24417a36c4d50d4d
Nick Dunkman <nickdunkman@gmail.com>
2025-11-28 12:01:26 -0800

    symlinks don't work

--- firefox/install.sh
+++ firefox/install.sh
@@ -3,10 +3,14 @@ for profile_directory in "${profile_directories[@]}"
 do
     # Create/replace a symlink to user.js
     rm -f ~/Library/Application\ Support/Firefox/Profiles/$profile_directory/user.js
-    ln -s "$PWD/user.js" ~/Library/Application\ Support/Firefox/Profiles/$profile_directory/user.js
+    cp "$DOTFILES/firefox/user.js" ~/Library/Application\ Support/Firefox/Profiles/$profile_directory/user.js
 
     # Create/replace a symlink to chrome/userChrome.css
     mkdir ~/Library/Application\ Support/Firefox/Profiles/$profile_directory/chrome 2> /dev/null
     rm -f ~/Library/Application\ Support/Firefox/Profiles/$profile_directory/chrome/userChrome.css
-    ln -s "$PWD/userChrome.css" ~/Library/Application\ Support/Firefox/Profiles/$profile_directory/chrome/userChrome.css
+    cp "$DOTFILES/firefox/userChrome.css" ~/Library/Application\ Support/Firefox/Profiles/$profile_directory/chrome/userChrome.css
+
+    # Note: firefox refuses to follow symlinks in profile folders. So gotta copy these fuckers over instead.
+    #ln -s "$PWD/user.js" ~/Library/Application\ Support/Firefox/Profiles/$profile_directory/user.js
+    #ln -s "$PWD/userChrome.css" ~/Library/Application\ Support/Firefox/Profiles/$profile_directory/chrome/userChrome.css
 done
